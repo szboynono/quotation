f3ebb45fc54bbc39a6dbfb7768ec5ed318ce9365
szboynono <szboylbd@hotmail.com>
2022-03-04 13:46:58 -0500

    add form validator

--- pages/create-quote.tsx
+++ pages/create-quote.tsx
@@ -1,59 +1,123 @@
 import React, { useCallback, useRef, useState } from "react";
 import { FaQuoteLeft } from "react-icons/fa";
-import domtoimage from 'dom-to-image';
+import domtoimage from "dom-to-image";
 
 const charLimit = 365;
 
 function CreateQuote() {
-  const [quote, setQuote] = useState("Say something");
-  const [name, setName] = useState("Satoshi Nakamoto");
+  const [quote, setQuote] = useState({
+    value: "Say Something",
+    error: false,
+  });
+  const [name, setName] = useState({
+    value: "Satoshi Nakamoto",
+    error: false,
+  });
 
-  const ref = useRef<HTMLDivElement>(null)
+  const ref = useRef<HTMLDivElement>(null);
 
-  const onButtonClick = useCallback(() => {
-    if (ref.current === null) {
-      return
-    }
-    domtoimage.toPng(ref.current, { width: 800, height: 600})
-    .then(function (dataUrl) {
-        var img = new Image();
-        img.src = dataUrl;
-        document.body.appendChild(img);
-    })
-    .catch(function (error) {
-        console.error('oops, something went wrong!', error);
-    });
-  }, [ref])
+  const onQuoteChange = (e: any) => {
+    setQuote((state) => ({
+      ...state,
+      value: e.target.value,
+      error: !e.target.value.length
+    }));
+  };
+
+  const onNameChange = (e: any) => {
+    setName((state) => ({
+      ...state,
+      value: e.target.value,
+      error: !e.target.value.length
+    }));
+  };
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (ref.current === null) {
+        return;
+      }
+
+      if (!!quote.error || !!name.error) return ;
+
+      domtoimage
+        .toPng(ref.current, { width: 800, height: 600 })
+        .then(function (dataUrl) {
+          var img = new Image();
+          img.src = dataUrl;
+          document.body.appendChild(img);
+        })
+        .catch(function (error) {
+          console.error("oops, something went wrong!", error);
+        });
+    },
+    [ref, quote, name]
+  );
 
   return (
     <>
-    {quote.length}
-    <div className="flex flex-col justify-center items-center">
-      <div ref={ref} className="w-fit flex items-center justify-center">
-        <div className="flex flex-col justify-center items-center mx-auto text-center w-[780px] h-[580px] p-10">
-          <FaQuoteLeft className="mx-auto mb-10 shrink-0" size={48} />
-          <div className="overflow-hidden max-h-[380px] p-2">
-          <h2 className={`${quote.length < 85 ? 'text-5xl' : 'text-3xl'} font-manrope`} style={ {wordBreak: 'break-word'}}>
-            {quote}
-          </h2>
+      <div className="flex flex-col justify-center items-center">
+        <div ref={ref} className="w-fit flex items-center justify-center">
+          <div className="flex flex-col justify-center items-center mx-auto text-center w-[780px] h-[580px] p-10">
+            <FaQuoteLeft className="mx-auto mb-10 shrink-0" size={48} />
+            <div className="overflow-hidden max-h-[380px] p-2">
+              <h2
+                className={`${
+                  quote.value.length < 85 ? "text-5xl" : "text-3xl"
+                } font-manrope`}
+                style={{ wordBreak: "break-word" }}
+              >
+                {quote.value}
+              </h2>
+            </div>
+            <p
+              className={`${
+                quote.value.length < 85 ? "text-3xl" : "text-xl"
+              } text-center font-manrope mt-12`}
+            >
+              - {name.value}
+            </p>
           </div>
-          <p className={`${quote.length < 85 ? 'text-3xl' : 'text-xl'} text-center font-manrope mt-12`}>- {name}</p>
         </div>
       </div>
-    </div>
-    <div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col justify-center items-center gap-5"
+      >
+        <label htmlFor="quote" className="underline">
+          Enter your quote
+        </label>
         <textarea
-          placeholder="quoto"
+          id="quote"
+          name="quote"
+          value={quote.value}
+          className="border w-[500px] h-[180px] shadow appearance-none rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           maxLength={charLimit}
-          onChange={(e) => setQuote(e.target.value)}
+          onChange={(e) => onQuoteChange(e)}
         />
+         {quote.error && <p className="text-red-500 text-xs italic">Please enter the quote.</p>}
+
+        <label htmlFor="name" className="underline">
+          Who said it
+        </label>
         <input
+          id="name"
+          name="name"
+          value={name.value}
+          className="border w-[500px] shadow appearance-none rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
-          placeholder="name"
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => onNameChange(e)}
         />
-        <button onClick={onButtonClick}>Mint</button>
-      </div>
+        {name.error && <p className="text-red-500 text-xs italic">Please enter the name.</p>}
+
+        <button
+          onClick={(e) => handleSubmit(e)}
+          className="text-xl border px-4 rounded-full font-bold bg-pink-400 text-white h-full hover:bg-pink-600"
+        >
+          Mint
+        </button>
+      </form>
     </>
   );
 }
