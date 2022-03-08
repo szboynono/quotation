8b54f3172fabe750dd229b17cbb87aa54c15dcee
szboynono <szboylbd@hotmail.com>
2022-03-08 14:19:07 -0500

    fix lint

--- pages/created.tsx
+++ pages/created.tsx
@@ -3,23 +3,23 @@ import { debounce } from "lodash";
 import Confetti from "react-confetti";
 import { AppContext } from "../context/AppContext";
 
-function created() {
-  const { id, currentNetwork } = useContext(AppContext);
+function Created() {
   const [windowSize, setWindowSize] = useState({
     height: 0,
     width: 0,
   });
 
-  const debouncedSetWindowSize = debounce(
-    () =>
-      setWindowSize({
-        height: window.innerHeight,
-        width: window.innerWidth,
-      }),
-    800
-  );
+  const { id, currentNetwork } = useContext(AppContext);
 
   useEffect(() => {
+    const debouncedSetWindowSize = debounce(
+      () =>
+        setWindowSize({
+          height: window.innerHeight,
+          width: window.innerWidth,
+        }),
+      800
+    );
     setWindowSize({
       height: window.innerHeight,
       width: window.innerWidth,
@@ -42,17 +42,23 @@ function created() {
           created a quote on the blockchain!
         </h1>
         <div className="flex flex-col text-center mt-40">
-        <a
-          href={`https://${currentNetwork === '0x4' ? 'testnets.' : ''}opensea.io/assets/0xcba687550c4cb2b737f69826eb2b158697aea754/${id}`}
-          className="text-lg underline hover:decoration-pink-400 mb-4"
+          <a
+            href={`https://${
+              currentNetwork === "0x4" ? "testnets." : ""
+            }opensea.io/assets/0xcba687550c4cb2b737f69826eb2b158697aea754/${id}`}
+            className="text-lg underline hover:decoration-pink-400 mb-4"
           >
-          View on opensea
-        </a>
-        <p className="text-gray-800 text-xs">Note: it may take a minute or two for opensea to show the image, please try to refresh the metadata in opensea if you can't see the image.</p>
+            View on opensea
+          </a>
+          <p className="text-gray-800 text-xs">
+            Note: it may take a minute or two for opensea to show the image,
+            please try to refresh the metadata in opensea if you can&apos;t see the
+            image.
+          </p>
         </div>
       </div>
     </div>
   );
 }
 
-export default created;
+export default Created;
