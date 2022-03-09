1daa3d696d9b566bdd470ad65a2de2efeb946bd3
szboynono <szboylbd@hotmail.com>
2022-03-08 19:09:19 -0500

    added meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,23 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import { AppContextProvider } from "../context/AppContext";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-
   return (
-    <AppContextProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AppContextProvider>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+        <title>Quotation - Create a quote NFT on ethereum</title>
+        <meta name="description" content="This is where you can create a one and only quote NFT on the ethereum blockchain" />
+        <meta name="keywords" content="NFT, ethereum, quotes, quotation, gift, memory, metamask, mint, trade, opensea" />
+      </Head>
+      <AppContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AppContextProvider>
+    </>
   );
 }
 
